b35e0518484a92c79253bacbe9642da060f82c74
Weiner Oliveira <woliveira82@gmail.com>
2022-12-11 22:10:52 +0000

    refact: src dir and resources

--- src/Player.gd
+++ src/Player.gd
@@ -1,13 +1,7 @@
 extends KinematicBody2D
 class_name Player
 
-export(int) var JUMP_FORCE = -160
-export(int) var JUMP_RELEASE_FORCE = -40
-export(int) var MAX_SPEED = 75
-export(int) var ACCELERATION = 10 
-export(int) var FRICTION = 10
-export(int) var GRAVITY = 5
-export(int) var ADDITIONAL_FALL_GRAVITY = 2
+export(Resource) var moveData
 
 var velocity = Vector2.ZERO
 
@@ -15,7 +9,7 @@ onready var animatedSprite = $AnimatedSprite
 
 
 func _ready():
-	animatedSprite.frames = load("res://game/sprite frames/GreenCharacter.tres")
+	animatedSprite.frames = load("res://src/resources/GreenCharacter.tres")
 
 
 func _physics_process(delta):
@@ -34,15 +28,15 @@ func _physics_process(delta):
 	
 	if is_on_floor():
 		if Input.is_action_pressed("ui_up"):
-			velocity.y = JUMP_FORCE
+			velocity.y = moveData.JUMP_FORCE
 		
 	else:
 		animatedSprite.animation = "Jump"
-		if Input.is_action_just_released("ui_up") and velocity.y < JUMP_RELEASE_FORCE:
-			velocity.y = JUMP_RELEASE_FORCE
+		if Input.is_action_just_released("ui_up") and velocity.y < moveData.JUMP_RELEASE_FORCE:
+			velocity.y = moveData.JUMP_RELEASE_FORCE
 		
 		if velocity.y > 0:
-			velocity.y += ADDITIONAL_FALL_GRAVITY
+			velocity.y += moveData.ADDITIONAL_FALL_GRAVITY
 
 	var was_in_air = not is_on_floor()
 	velocity = move_and_slide(velocity, Vector2.UP)
@@ -52,13 +46,13 @@ func _physics_process(delta):
 
 
 func apply_gravity():
-	velocity.y += GRAVITY
+	velocity.y += moveData.GRAVITY
 	velocity.y = min(velocity.y, 300)
 
 
 func apply_friction():
-	velocity.x = move_toward(velocity.x, 0, FRICTION)
+	velocity.x = move_toward(velocity.x, 0, moveData.FRICTION)
 
 
 func apply_acceleration(amount):
-	velocity.x = move_toward(velocity.x, MAX_SPEED * amount, ACCELERATION)
+	velocity.x = move_toward(velocity.x, moveData.MAX_SPEED * amount, moveData.ACCELERATION)
