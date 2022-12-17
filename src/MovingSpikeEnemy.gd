e45455b951004c53bc24cf37d5fbb37fc2729496
Weiner Oliveira <woliveira82@gmail.com>
2022-12-17 22:07:28 +0000

    feat: add moving spike enemy

---
+++ src/MovingSpikeEnemy.gd
@@ -0,0 +1,19 @@
+extends Path2D
+
+enum ANIMATION_TYPE {
+	LOOP,
+	BOUCE
+}
+
+export(ANIMATION_TYPE) var animation_type
+export(float) var speed = 1
+
+onready var animationPlayer = $AnimationPlayer
+
+
+func _ready():
+	match animation_type:
+		ANIMATION_TYPE.LOOP: animationPlayer.play("MoveAlongPathBounce")
+		ANIMATION_TYPE.BOUCE: animationPlayer.play("BounceAlongPathBounce")
+	
+	animationPlayer.playback_speed = speed
