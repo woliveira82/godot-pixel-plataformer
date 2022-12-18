288fdd948f7bab0cf50d2e4db4807590bf4f758b
Weiner Oliveira <woliveira82@gmail.com>
2022-12-18 22:03:10 +0000

    fix: apply delta in player movement

--- src/Player.gd
+++ src/Player.gd
@@ -32,7 +32,7 @@ func _physics_process(delta):
 	input.y = Input.get_axis("ui_up", "ui_down")
 	
 	match state:
-		MOVE: move_state(input)
+		MOVE: move_state(input, delta)
 		CLIMB: climb_state(input)
 
 
@@ -43,18 +43,18 @@ func is_on_ladder():
 	return true
 
 
-func move_state(input):
+func move_state(input, delta):
 	if is_on_ladder() and Input.is_action_pressed("ui_up"):
 		state = CLIMB
 
-	apply_gravity()
+	apply_gravity(delta)
 	
 	if input.x == 0:
-		apply_friction()
+		apply_friction(delta)
 		animatedSprite.animation = "Idle"
 	
 	else:
-		apply_acceleration(input.x)
+		apply_acceleration(input.x, delta)
 		animatedSprite.animation = "Run"
 		animatedSprite.flip_h = input.x > 0
 	
@@ -71,7 +71,7 @@ func move_state(input):
 		input_jump_release()
 		input_double_jump()
 		buffer_jump()
-		fast_fall()
+		fast_fall(delta)
 
 	var was_on_floor = is_on_floor()
 	velocity = move_and_slide(velocity, Vector2.UP)
@@ -117,9 +117,9 @@ func buffer_jump():
 		jumpBufferTimer.start()
 
 
-func fast_fall():
+func fast_fall(delta):
 	if velocity.y > 0:
-		velocity.y += moveData.ADDITIONAL_FALL_GRAVITY
+		velocity.y += moveData.ADDITIONAL_FALL_GRAVITY * delta
 
 
 func input_jump():
@@ -138,17 +138,17 @@ func climb_state(input):
 	velocity = move_and_slide(velocity, Vector2.UP)
 
 
-func apply_gravity():
-	velocity.y += moveData.GRAVITY
+func apply_gravity(delta):
+	velocity.y += moveData.GRAVITY * delta
 	velocity.y = min(velocity.y, 300)
 
 
-func apply_friction():
-	velocity.x = move_toward(velocity.x, 0, moveData.FRICTION)
+func apply_friction(delta):
+	velocity.x = move_toward(velocity.x, 0, moveData.FRICTION * delta)
 
 
-func apply_acceleration(amount):
-	velocity.x = move_toward(velocity.x, moveData.MAX_SPEED * amount, moveData.ACCELERATION)
+func apply_acceleration(amount, delta):
+	velocity.x = move_toward(velocity.x, moveData.MAX_SPEED * amount, moveData.ACCELERATION * delta)
 
 
 func _on_JumpBufferTimer_timeout():
